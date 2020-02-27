efb50cbf139f7c7b12c9f69b13978f471eab4a4b
Soumi Bhattacharyya <bhattasoumi@gmail.com>
2020-02-27 17:44:08 +0530

    New Test File

---
+++ Core-Java/src/com/tyss/capgemini/loops/DoWhileLoop2.java
@@ -0,0 +1,19 @@
+package com.tyss.capgemini.loops;
+
+public class DoWhileLoop2 {
+	
+	
+		public static void main(String[] args) {
+
+			int i = 10;
+
+			do { 			// in do-while whatever the condition might be , the loop runs atleast once and
+							// the value gets printed in the first time.
+				System.out.println(i * 18);
+				i++;
+			} while (i <= 0);
+		}
+
+	}
+
+}
